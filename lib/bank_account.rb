f0dfe2e5c2ed9d17220ce484a3937bb20a4e023a
ejgann <aspentree79@gmail.com>
2019-10-31 17:54:11 -0400

    end of day

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class BankAccount
+
+    attr_reader :name 
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(money)
+        self.balance = money + balance
+    end
+
+    def display_balance
+        return "Your balance is $#{balance}."
+    end
+
+    def valid?
+        return status == "open" && balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+ 
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid? 
+  end
+
+  def execute_transfer
+
+  end
 end
